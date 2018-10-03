4c372b4ff5132a102d44ea3d9ed9b3e2f09923d5
danilashamin <danilashamin@gmail.com>
2018-10-03 14:23:43 +0300

    onerror in login listener

--- app/src/main/java/me/askfix/api/askfix/model/LoginListener.kt
+++ app/src/main/java/me/askfix/api/askfix/model/LoginListener.kt
@@ -1,5 +1,8 @@
 package me.askfix.api.askfix.model
 
+import retrofit2.Call
+
 interface LoginListener {
     fun onLoginResponse(loginResponse: LoginResponse?)
+    fun onError(call: Call<LoginResponse>, t: Throwable)
 }
