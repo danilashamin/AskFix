9301e1720cec2b32412b69821ea5d347e9a31eeb
danilashamin <danilashamin@gmail.com>
2018-10-05 16:15:40 +0300

    no need handlers, using anonymous classes

--- app/src/main/java/me/askfix/api/askfix/centrifuge/OnPublishListener.java
+++
@@ -1,8 +0,0 @@
-package me.askfix.api.askfix.centrifuge;
-
-import centrifuge.PublishEvent;
-import centrifuge.Subscription;
-
-public interface OnPublishListener {
-    void onPublish(final Subscription sub, final PublishEvent event);
-}
